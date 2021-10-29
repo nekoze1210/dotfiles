a5e447ecfabb3a66a360a2f5f15872e56e0e5038
Daiki Nagaoka <neko.z.n1210@gmail.com>
2021-10-29 09:42:25 +0900

    :+1: install Vundle if directory does not exists when bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,15 @@ xcode-select -v >/dev/null 2>&1 || xcode-select --install
 
 echo "install homebrew"
 which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "run brew doctor "
 which brew >/dev/null 2>&1 && brew doctor
+echo "run brew update --verbose"
 which brew >/dev/null 2>&1 && brew update --verbose
 
 echo "run homebrew bundle"
 brew bundle -v --file=Brewfile --no-lock
+
+echo "install vundle"
+[ -d ~/.dotfiles/.vim/bundle/Vundle.vim ]  || git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/.vim/bundle/Vundle.vim
 
 echo "all done."
