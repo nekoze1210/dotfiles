a61325d4a6e1a2cdf09b647b0b2236768c860490
Daiki Nagaoka <neko.z.n1210@gmail.com>
2021-11-16 00:36:41 +0900

    :+1: add importing iterm and karabiner settings to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,4 +20,12 @@ brew bundle -v --file=Brewfile --no-lock
 echo "install vundle"
 [ -d ~/.dotfiles/.vim/bundle/Vundle.vim ]  || git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/.vim/bundle/Vundle.vim
 
+echo "import Karabiner-Elements Settings"
+brew info karabiner-elements >/dev/null 2>&1 && cp "$(pwd)/karabiner/config.json" ~/.config/karabiner/karabiner.json
+
+echo "download iterm2 color presets"
+curl -o ~/Downloads/iTerm2Color/Hardcore.itermcolors  https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Hardcore.itermcolors --create-dirs
+echo "downloaded iterm2 color presets at ~/Downloads/iTerm2Color/"
+echo "[notice] You can change color presets by clicking import at Preferences > profiles > colors."
+
 echo "all done."
