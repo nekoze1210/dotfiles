9181d4311c3f5cceeddfef2a74c38bad12d30491
Daiki Nagaoka <neko.z.n1210@gmail.com>
2021-11-20 21:57:40 +0900

    :+1: skip .DS_Store file to symbolic link

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ for f in $dotfiles; do
   [ "$f" = ".gitconfig.local.template" ] && continue
   [ "$f" = ".require_oh-my-zsh" ] && continue
   [ "$f" = ".gitmodules" ] && continue
+  [ "$f" = ".DS_Store" ] && continue
   
   ln -snfv "$DOTPATH"/"$f" "$HOME"/"$f"
 done
