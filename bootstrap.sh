ca58164d2a9a5595af4e90b47a08f91977d5ffa4
nekoze <14988862+nekoze1210@users.noreply.github.com>
2026-03-24 21:59:51 +0900

    chore: remove rosetta2 installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,11 +21,6 @@ defaults write com.apple.menuextra.clock ShowSeconds -bool true
 echo "install xcode-select"
 xcode-select -v >/dev/null 2>&1 || xcode-select --install
 
-echo "install Rosetta2"
-if [ arch = "arm64" ]; then
-    /usr/bin/pgrep oahd >/dev/null 2>&1 || /usr/sbin/softwareupdate –install-rosetta –agree-to-license
-fi
-
 echo "install homebrew"
 which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "run brew doctor "
@@ -34,7 +29,7 @@ echo "run brew update --verbose"
 which brew >/dev/null 2>&1 && brew update --verbose
 
 echo "run homebrew bundle"
-brew bundle -v --file=Brewfile --no-lock
+brew bundle -v --file=Brewfile
 
 echo "install vundle"
 [ -d ~/.dotfiles/.vim/bundle/Vundle.vim ]  || git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/.vim/bundle/Vundle.vim
